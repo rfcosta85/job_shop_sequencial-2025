ae0d6d341d1fbd05264c1f80f357448056c55b16
Rodrigo <rfcosta85@gmail.com>
2025-05-10 11:33:09 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/job_shop_sequencial.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/job_shop_sequencial.dir/main.c.obj"
+  "CMakeFiles/job_shop_sequencial.dir/main.c.obj.d"
+  "job_shop_sequencial.exe"
+  "job_shop_sequencial.exe.manifest"
+  "job_shop_sequencial.pdb"
+  "libjob_shop_sequencial.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/job_shop_sequencial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/job_shop_sequencial.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for job_shop_sequencial.
+# This may be replaced when dependencies are built.
